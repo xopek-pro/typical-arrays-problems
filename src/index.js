dbda1a1227dbce230fa71a805a55344305021d8c
xopek-pro <xopek.pro@internet.ru>
2021-03-11 17:16:43 +0300

    feat: implement max, min, and avg functions for searching minimal value, maximal value, and an average value of a given array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) {  
+  if (array === undefined || array.length==0){
+    return 0;
+  } else {
+    let minValue=array[0];
+    for (let index = 1; index < array.length; index++) {
+      if (array[index] < minValue){
+        minValue = array[index];
+      };      
+    }
+    return minValue;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length==0){
+    return 0;
+  } else {
+    let maxValue=array[0];
+    for (let index = 1; index < array.length; index++) {
+      if (array[index] > maxValue){
+        maxValue = array[index];
+      };      
+    }
+    return maxValue;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length==0){
+    return 0;
+  } else {
+    let sum=0;
+    for (let index = 0; index < array.length; index++) {
+        sum += array[index];
+    }
+    return sum/array.length;
+  }
 }
